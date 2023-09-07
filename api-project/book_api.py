ca380a78cdc36c0f64b0486e341d3bc04acfdbcf
Aviad Yarkoni <Aviad.Yarkoni@outlook.com>
2023-09-07 06:15:58 -0400

    read JSON File

--- api-project/book_api.py
+++ api-project/book_api.py
@@ -1,25 +1,43 @@
 
 from fastapi import FastAPI
-import labary_books 
+import json
+
+
+# app = FastAPI()
+# def main_book_api ():
+#     labrary_books_file = "~/scripts/api-project/labrary_books"
+#     labrary_books = load_json_file("a")
+
+
+def read_json_file():
+    json_file = "/home/osboxes/scripts/api-project/labrary_books.json"
+    with open(json_file, "r") as file:
+        data = json.load(file)  
+    return data
+
+
+def read_json_file2():
+    json_file = "/home/osboxes/scripts/api-project/labrary_books.json"
+    file = open(json_file)
+    data = json.load(file)  
+    return data
+  
+# @app.get("/")
+# def root():
+#     return {"message": "Hello book"}
+
+
+# @app.get("/book/{book_name}")
+# def get_book_name (book_name):
+#     return {"book":book_name }
+
+
+a = read_json_file2()
+print (a)
+json_file = "/home/osboxes/scripts/api-project/labrary_books.json"
 
 
 
-
-app = FastAPI()
-
-
-@app.get("/")
-def root():
-    return {"message": "Hello book"}
-
-
-@app.get("/book/{book_name}")
-def get_book_name (book_name):
-    return {"book":book_name }
-
-
-
-
-#for web server run shell below
+#run web server 
 #uvicorn book_api:app --reload
 
