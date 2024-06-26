c6adb4657bb48dbfeae0cb49d16c299ae09a6017
Aviad Yarkoni <Aviad.Yarkoni@outlook.com>
2024-06-26 16:38:04 +0300

    ghxgfgjfc

--- python/guest-the-number/guest-the-number.py
+++ python/guest-the-number/guest-the-number.py
@@ -2,13 +2,22 @@ import random
 
 def guess (high_numbr):
     random_number= random.randint(1,high_numbr)
-    guess = 0 
-    if guess == random_number:
+    result = 1
+    while result !=0:
+        guess = input ('insert your guss number between 1 - ', random_number)
+        result = check_number(guess,random_number)
+        if result ==1:
+            print ('too low , guest again')
+        if result ==2:
+            print ('too high, guest again')
+
+    print ('congrats,you have geasted the number {random_number} correctly')
+
+def check_number (guess,corect_number):
+    if guess == corect_number:
         return 0
-    if guess < random_number:
+    if guess < corect_number:
         return 1
-    if guess > random_number:
+    if guess > corect_number:
         return 2
-
-
 
