ea3682ee96f4a79001c5cc9b2333dafa2825d7d8
Aviad Yarkoni <Aviad.Yarkoni@outlook.com>
2024-06-30 20:44:06 +0300

    final code for project

--- python/guest-the-number/guest-the-number.py
+++ python/guest-the-number/guest-the-number.py
@@ -4,14 +4,18 @@ def guess (high_numbr):
     random_number= random.randint(1,high_numbr)
     result = 1
     while result !=0:
-        guess = input ('insert your guss number between 1 - ', random_number)
+        guess = int(input(f'insert your guess number between 1 - {high_numbr}:'))
         result = check_number(guess,random_number)
         if result ==1:
             print ('too low , guest again')
+            guess = int(input(f'insert your guess number between 1 - {high_numbr}:'))
+            result = check_number(guess,random_number)
         if result ==2:
             print ('too high, guest again')
+            guess = int(input(f'insert your guess number between 1 - {high_numbr}:'))
+            result = check_number(guess,random_number)
 
-    print ('congrats,you have geasted the number {random_number} correctly')
+    print (f'congrats,you have geasted the number {random_number} correctly')
 
 def check_number (guess,corect_number):
     if guess == corect_number:
@@ -23,5 +27,5 @@ def check_number (guess,corect_number):
 
 
 
-higher_numbr= input ('insert number ')
+higher_numbr= int ( input ('insert number '))
 guess (higher_numbr)
