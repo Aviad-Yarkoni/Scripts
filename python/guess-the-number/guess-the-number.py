4e8bf6bdc66d15f2d2f867220b28d4ba3dea2963
Aviad Yarkoni <Aviad.Yarkoni@outlook.com>
2024-07-01 09:20:46 +0300

    asef# Please enter the commit message for your changes. Lines starting

--- python/guess-the-number/guess-the-number.py
+++ python/guess-the-number/guess-the-number.py
@@ -2,28 +2,22 @@ import random
 
 def guess (high_numbr):
     random_number= random.randint(1,high_numbr)
-    result = 1
-    while result !=0:
-        guess = int(input(f'insert your guess number between 1 - {high_numbr}:'))
-        result = check_number(guess,random_number)
-        if result ==1:
-            print ('too low , guess again')
-            guess = int(input(f'insert your guess number between 1 - {high_numbr}:'))
-            result = check_number(guess,random_number)
-        if result ==2:
-            print ('too high, guess again')
-            guess = int(input(f'insert your guess number between 1 - {high_numbr}:'))
-            result = check_number(guess,random_number)
+    is_correct= False
 
+    while is_correct==False:
+        guess = int(input(f'insert your guess number between 1 - {high_numbr}:'))
+        is_correct = check_number(guess,random_number)       
     print (f'congrats,you have geassed the number {random_number} correctly')
 
 def check_number (guess,corect_number):
     if guess == corect_number:
-        return 0
+        return True
     if guess < corect_number:
-        return 1
+        print ('too low , guess again')
+        return False
     if guess > corect_number:
-        return 2
+        print ('too high, guess again')
+        return False
 
 
 
